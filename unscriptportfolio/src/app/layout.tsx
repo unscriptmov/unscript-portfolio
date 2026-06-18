7658e24e7b61fc884f3459f763800f99d83f218c
Nicolás Avilés <avilesarayan@gmail.com>
2026-06-18 17:51:15 -0400

    Google Analytics 4

--- unscriptportfolio/src/app/layout.tsx
+++ unscriptportfolio/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import SmoothScroll from "./SmoothScroll";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; // 1. Importamos el componente de Next.js
 import "./globals.css";
 
 const geistSans = Geist({
@@ -16,7 +17,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Unscriptedream ┃ 2D Animator",
   description: "2D Animator",
-
   icons: {
     icon: "/images/favicon.ico",
   },
@@ -32,6 +32,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* 2. Añadimos los scripts de Google Analytics dentro del <head> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V1MCPT6XT0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V1MCPT6XT0');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-black text-white">
         <SmoothScroll />
         {children}
